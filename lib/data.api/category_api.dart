fe1fd7affd3960da3e674421a8d57d93ede6d2bf
BBarisKilic <bulentbariskilic@gmail.com>
2020-09-22 03:49:36 +0300

    Third Commit

--- lib/data.api/category_api.dart
+++ lib/data.api/category_api.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart' as http;
 
-class categoryApi {
+class CategoryApi {
   static Future getCategories() {
     return http.get("http://10.0.2.2:3000/categories");
   }
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MainScreenState();
+  }
+}
+
+class MainScreenState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Alışveriş Sistemi",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+      ),
+    );
+  }
+}
